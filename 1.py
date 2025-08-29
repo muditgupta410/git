127bac6671fd7a1db724798e1235fc551e246eee
Mudit Gupta <muditgupta410@gmail.com>
2025-08-29 16:35:13 -0300

    This is divison code

--- 1.py
+++ 1.py
@@ -5,12 +5,16 @@ def addition(a,b):
 def subtraction(a,b):
     print(a-b)
 
-addition(2,3)
-subtraction(7,2)
+
 
 def multiplication(a,b):
     print(a*b)
 
+
+def divison(a,b):
+    print(a/b)
+
 addition(2,3)
 subtraction(7,2)
 multiplication(5*2)
+divison(4/2)
